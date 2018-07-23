8a9195c62732d83d5e5e9cf4822da26ffb602152
Robin Zhang <rzhang19@users.noreply.github.com>
2018-07-23 04:39:05 -0400

    Updated source

--- swing1/HelloWorldFrame.java
+++ swing1/HelloWorldFrame.java
@@ -1,6 +1,10 @@
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
+/*
+ * From tutorial online at http://www.wideskills.com/java-tutorial/java-swing-tutorial
+ */
+
 public class HelloWorldFrame extends JFrame {
    public static void main (String[] args) {
       new HelloWorldFrame();
